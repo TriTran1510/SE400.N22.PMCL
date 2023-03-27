e08f8657ff4215989661213c0296af9a485878ac
20520327 <20520327@gm.uit.edu.vn>
2023-03-27 21:02:25 +0700

    Change database from singlestore to TiDB

--- SE400.N22.PMCL/MainWindow.xaml.cs
+++ SE400.N22.PMCL/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace SE400.N22.PMCL
 {
@@ -23,6 +26,19 @@ namespace SE400.N22.PMCL
         public MainWindow()
         {
             InitializeComponent();
+            string connectionString = ConfigurationManager.ConnectionStrings["TiDBConnectionString"].ConnectionString;
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                
+                    connection.Open();
+                    // Execute SQL queries or commands here
+                    MySqlCommand cmd = new MySqlCommand("SELECT high,low from MarketStock.TECHM limit 10;", connection);
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                    DataSet dataTable = new DataSet();
+                    adapter.Fill( dataTable, "LoadDataBinding");
+                    dataGridCustomers.DataContext = dataTable;
+                
+            }
         }
     }
 }
--- SE400.N22.PMCL/obj/Debug/net6.0-windows/App.g.i.cs
+++ SE400.N22.PMCL/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace SE400.N22.PMCL {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/SE400.N22.PMCL;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SE400.N22.PMCL;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
